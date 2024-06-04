4678c66c3c721bbff23730d4c2ab41df8814c24c
NahumMorales <eliabnahumml@gmail.com>
2024-06-04 17:01:38 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,33 @@
+var sea
+var ship
+
 
 function preload(){
-
+seaImg = loadImage("sea.png");
+shipImg1 = loadAnimation ("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
 }
+   
+  
+
+
+
 
 function setup(){
-  createCanvas(400,400);
-  
+createCanvas(400,400);
+sea=createSprite(1,200,400,5);
+
+sea.addImage (seaImg);
+sea.scale=0.3;
+ship=createSprite(100,299,100,100);
+ship.addAnimation("barco",shipImg1)
+ship.scale=0.2
+
 }
 
 function draw() {
-  background("blue");
- 
-}
+  background("green");
+  drawSprites();
+  if(sea.x<0){
+    sea.x=sea.width/2;}
+   
+   }
